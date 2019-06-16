e10a8ba6feb08cb8c12794168c72ce2c18c45efd
Jan Pikl <pikl.jan@gmail.com>
2019-06-16 02:29:22 +0200

    Use getopts instead of getopt

--- .local/lib/bash/args.sh
+++ .local/lib/bash/args.sh
@@ -1,76 +1,21 @@
 # shellcheck shell=bash
 
-readonly ERR_NO_GNU_GETOPT=10
-readonly ERR_UNPROCESSABLE_ARGS=11
-readonly ERR_INVALID_ARGS=12
-readonly ERR_MISSING_ARGS=13
-
-readonly PURE_GETOPT_DIR=$BASH_LIB_DIR/pure-getopt
-readonly PURE_GETOPT_SCRIPT=$PURE_GETOPT_DIR/getopt.bash
-
 help_usage() {
-  echo "Try '$(self) --help' for more information."
+  echo "Try '$(self) -h' for more information."
 }
 
-die_no_gnu_getopt() {
-  {
-    alert "GNU getopt is required"
-    echo "You can get pure bash implementation of GNU getopt with:"
-    echo "  dotfiles submodule update $PURE_GETOPT_DIR"
-  } >&2
-  exit $ERR_NO_GNU_GETOPT
-}
+die_invalid_opt() {
+  if [ "$1" == : ]; then
+    alert "option -$OPTARG requires an argument"
+  else
+    alert "invalid option -$OPTARG"
+  fi
+  help_usage
+  die
+} >&2
 
-die_unprocessable_args() {
-  alert "internal error: unable to process arguments" >&2
-  exit $ERR_UNPROCESSABLE_ARGS
-}
-
-die_invalid_args() {
-  help_usage >&2
-  exit $ERR_INVALID_ARGS
-}
-
-die_missing_args() {
-  {
-    alert "missing required argument"
-    help_usage
-  } >&2
-  exit $ERR_MISSING_ARGS
-}
-
-is_gnu_getopt() {
-  local result=0
-  getopt --test &>/dev/null || result=$?
-  [[ $result -eq 4 ]]
-}
-
-if is_gnu_getopt; then
-  call_getopt() {
-    getopt "$@"
-  }
-elif [[ -f $PURE_GETOPT_SCRIPT ]]; then
-  source "$PURE_GETOPT_SCRIPT"
-
-  call_getopt() (
-    set +euo pipefail # pure-getopt does not support strict mode.
-    getopt "$@"
-  )
-else
-  die_no_gnu_getopt
-fi
-
-parse_args() {
-  local -r short_opts=$1
-  local -r long_opts=$2
-  shift 2
-  call_getopt \
-    --name "$(self)" \
-    --options "$short_opts" \
-    --longoptions "$long_opts" \
-    -- "$@"
-}
-
-get_args() {
-  parse_args "$@" || die_invalid_args
-}
+die_missing_arg() {
+  alert "missing required argument"
+  help_usage
+  die
+} >&2
--- .local/lib/bash/core.sh
+++ .local/lib/bash/core.sh
@@ -1,17 +1,15 @@
 # shellcheck shell=bash
 
-readonly ERR_GENERIC=1
-
 self() {
   basename "$0"
 }
 
-die() {
-  [[ "${1:-}" ]] && alert "$1" >&2
-  exit "${2:-$ERR_GENERIC}"
-}
-
 alert() {
   echo "$(self): $1"
+}
+
+die() {
+  [[ "${1:-}" ]] && alert "$1" >&2
+  exit 1
 }
 
