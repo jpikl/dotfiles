69a40ce0443e5a992c41e39b24f2c63df81bd478
Jan Pikl <pikl.jan@gmail.com>
2019-10-13 23:57:41 +0200

    Merge args.sh to core.sh

--- .local/lib/bash/core.sh
+++ .local/lib/bash/core.sh
@@ -16,3 +16,21 @@ die() {
   [[ "${1:-}" ]] && alert "$1"
   exit $ERR_GENERIC
 } >&2
+
+die_help() {
+  alert "$1"
+  echo "Try '$(self) -h' for more information."
+  exit $ERR_GENERIC
+} >&2
+
+die_invalid_opt() {
+  if [ "$1" == : ]; then
+    die_help "option -$OPTARG requires an argument"
+  else
+    die_help "invalid option -$OPTARG"
+  fi
+}
+
+die_missing_arg() {
+  die_help "missing required argument"
+}
