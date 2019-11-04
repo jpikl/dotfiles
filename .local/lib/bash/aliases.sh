2b9d90b5e9d8c72f3948075f45ac820e9a8481e6
Jan Pikl <pikl.jan@gmail.com>
2019-11-04 10:45:42 +0100

    Reorganize aliases

--- .local/lib/bash/aliases.sh
+++ .local/lib/bash/aliases.sh
@@ -3,6 +3,13 @@
 # Git for dotfiles
 alias dotfiles='git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME'
 
+# Backup
+alias bakmusls='dirtree -s $MUSIC_DIR > $BACKUP_DIR/music.txt'
+
+# Bat
+alias batfzf='bat $(find -type f | fzfbat)'
+alias batfzfsrc='bat $(findsrc -type f | fzfbat)'
+
 # Ls
 alias ls='ls --color=auto'
 alias la='ls --almost-all'
@@ -13,91 +20,68 @@ alias lla='ll --almost-all'
 alias cdbak='cd "$BACKUP_DIR"'
 alias cdbin='cd "$LOCAL_BIN_DIR"'
 alias cdcach='cd "$CACHE_DIR"'
-alias cdcfg='cd "$CONFIG_DIR"'
-alias cddata='cd "$LOCAL_SHARE_DIR"'
+alias cdcfg='cdconf'
+alias cdconf='cd "$CONFIG_DIR"'
+alias cddat='cd "$LOCAL_SHARE_DIR"'
 alias cddesk='cd "$DESKTOP_DIR"'
 alias cddoc='cd "$DOCUMENTS_DIR"'
+alias cddok='cddoc'
 alias cddown='cd "$DOWNLOAD_DIR"'
+alias cddsk='cddesk'
+alias cddwn='cddown'
+alias cdfzf='cd $(find -type d | fzfls)'
+alias cdfzfsrc='cd $(findsrc -type d | fzfls)'
 alias cdlib='cd "$LOCAL_LIB_DIR"'
 alias cdmed='cd "$MEDIA_DIR"'
 alias cdmus='cd "$MUSIC_DIR"'
 alias cdmvnroot='cd "$(mvnroot)"'
 alias cdnpmdir='cd "$(npmdirs -Z | choose -izp "NPM directory:")"'
 alias cdpic='cd "$PICTURES_DIR"'
-alias cdplst='cd "$PLAYLISTS_DIR"'
-alias cdprv='cd "$PRIVATE_DECRYPTED_DIR"'
+alias cdplay='cd "$PLAYLISTS_DIR"'
+alias cdplst='cdplay'
+alias cdpriv='cd "$PRIVATE_DECRYPTED_DIR"'
+alias cdprv='cdpriv'
 alias cdpub='cd "$PUBLIC_DIR"'
+alias cdtemp='cd "$TEMP_DIR"'
 alias cdtmp='cd "$TEMP_DIR"'
 alias cdvid='cd "$VIDEOS_DIR"'
 alias cdvol='cd "$VOLUMES_DIR"'
 alias cdwork='cd "$WORKSPACE_DIR"'
+alias cdwrk='cdwork'
+
+# Clipboard
+alias clipi='xclip -in -selection clipboard'
+alias clipo='xclip -out -selection clipboard'
+
+# Exif
+alias exiffzf='exiftool $(find -type f | fzfexif)'
 
 # Grep
 alias grep='grep --color=auto'
-alias egrep='grep --extended-regexp'
-alias fgrep='grep --fixed-strings'
+alias grep='grep --perl-regexp'
+alias grepe='grep --extended-regexp'
+alias grepf='grep --fixed-strings'
 
 # Find
 alias findsrc='find $(findexpr -s)'
-
-# Clipboard
-alias wclip='xclip -in -selection clipboard'
-alias rclip='xclip -out -selection clipboard'
 
 # Fzf
 alias fzfbat='fzf --preview="bat --color=always {}"'
 alias fzfls='fzf --preview="ls -l --almost-all --human-readable --color=always {}"'
 alias fzfexif='fzf --preview="exiftool {}"'
 alias fzfxargs='fzf --multi --read0 --print0 | xargs --delimiter="\0" --no-run-if-empty'
-
-# Fzf (inverse)
-alias batfzf='bat $(find -type f | fzfbat)'
-alias batfzfsrc='bat $(findsrc -type f | fzfbat)'
-alias cdfzf='cd $(find -type d | fzfls)'
-alias cdfzfsrc='cd $(findsrc -type d | fzfls)'
-alias exiffzf='exiftool $(find -type f | fzfexif)'
-alias xargsfzf='find -print0 | fzfxargs'
-alias xargsfzfsrc='findsrc -print0 | fzfxargs'
+# shellcheck disable=SC2142
+alias fzfpac='fzf --multi --no-sort --ansi --preview="pacaur -Si {2}" | awk "{print \$2}"'
 
 # Git
 alias gitcb='git checkout "$(gitlsb -lrx | choose -ip "Git branch to checkout:")"'
 alias gitct='git checkout "$(git tag | choose -ip "Git tag to checkout:")"'
+alias gitDb='gitdb --force'
+alias gitdb='git branch --delete "$(gitlsb -l | choose -imp "Git branches to delete:")"'
 alias gitfap='git fetch --all --prune'
 alias gitffb='gitff $(gitlsb -l | choose -imp "Git branches to fast-forward:")'
 alias gitladog='git log --all --decorate --oneline --graph'
-alias gitmb='git merge "$(gitlsb -lr | choose -ip "Git branch to merge:")"'
-
-# Npm
-alias npmg='npm --global'
-alias npmgl='npmg --depth=0 list'
-alias npmgi='npmg install'
-alias npmgup='npmg update'
-alias npmgun='npmg uninstall'
-alias npmi='npm install'
-alias npmfi='rm --force package-lock.json && npmi'
-alias npmffi='rm --force --recursive node_modules && npmfi'
-alias npml='npm --depth=0 list'
-alias npmi='npm install'
-alias npmup='npm update'
-alias npmun='npm uninstall'
-alias npmd='npmr webpack-dev dev'
-
-# Maven
-alias mvni='mvn install'
-alias mvnist='mvn install -Dmaven.test.skip=true'
-alias mvnci='mvn clean install'
-alias mvncist='mvnci -Dmaven.test.skip=true'
-
-# Pacman / pacaur
-alias pacref='pacaur -Sy'
-alias pacup='pacaur -Syu'
-# shellcheck disable=SC2142
-alias pacsel='fzf --multi --no-sort --ansi --preview="pacaur -Si {2}" | awk "{print \$2}"'
-alias pacin='pacfind -ra | pacsel | xargs --no-run-if-empty retty pacaur -S'
-alias pacun='pacfind -i | pacsel | xargs --no-run-if-empty retty pacaur -Rsc'
-
-# React Native
-alias rndebug='adb shell input keyevent 82'
+alias gitmb='git merge "$(gitlsb -lr | choose -ip "Git branch to merge to \""$(gitlsb -c)"\":")"'
 
 # Iconv
 alias win1250-utf8='iconv --from-code=WINDOWS-1250 --to-code=UTF-8'
@@ -105,12 +89,42 @@ alias utf8-win1250='iconv --from-code=UTF-8 --to-code=WINDOWS-1250'
 alias iso88592-utf8='iconv --from-code=ISO-8859-2 --to-code=UTF-8'
 alias utf8-iso88592='iconv --from-code=UTF-8 --to-code=ISO-8859-2'
 
+# Npm
+alias npmd='npmr webpack-dev dev'
+alias npmg='npm --global'
+alias npmgi='npmg install'
+alias npmgl='npmg --depth=0 list'
+alias npmgun='npmg uninstall'
+alias npmgup='npmg update'
+alias npmi='npm install'
+alias npmii='rm --force --recursive node_modules && npmi'
+alias npml='npm --depth=0 list'
+alias npmun='npm uninstall'
+alias npmup='npm update'
+
+# Maven
+alias mvnci='mvn clean install'
+alias mvncist='mvnci -Dmaven.test.skip=true'
+alias mvni='mvn install'
+alias mvnist='mvni -Dmaven.test.skip=true'
+
+# Pacman / pacaur
+alias paci='pacfind -ra | fzfpac | xargs --no-run-if-empty retty pacaur -S'
+alias pacr='pacaur -Sy'
+alias pacri='pacr && paci'
+alias pacun='pacfind -i | fzfpac | xargs --no-run-if-empty retty pacaur -Rsc'
+alias pacup='pacaur -Syu'
+
+# React Native
+alias rndebug='adb shell input keyevent 82'
+
 # SSH
-alias sshinit='eval $(sshstart)'
-alias sshadd='ssh-add "$(choose -p "SSH key to add:" ~/.ssh/*.key)"'
+alias ssha='ssh-add "$(choose -p "SSH key to add:" ~/.ssh/*.key)"'
+alias sshi='eval $(sshstart)'
 
-# Backup
-alias bakmuslist='dirtree -s $MUSIC_DIR > $BACKUP_DIR/music.txt'
+# Terminal
+alias termfit='cut --characters=-$COLUMNS'
 
-# Other
-alias fitterm='cut --characters=-$COLUMNS'
+# Xargs
+alias xargsfzf='find -print0 | fzfxargs'
+alias xargsfzfsrc='findsrc -print0 | fzfxargs'
