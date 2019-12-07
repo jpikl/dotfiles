52e90f64c2085c1e19fe29dd60843cc34eff862c
Jan Pikl <pikl.jan@gmail.com>
2019-12-07 23:53:27 +0100

    Allow mpdm3u to pass dirs through options

---
+++ .local/lib/bash/path.sh
@@ -0,0 +1,17 @@
+# shellcheck shell=bash
+
+path_from_env() {
+  local path=${!1:-}
+  if [[ $path && $path != /* ]]; then
+    die "$1 variable must be an absolute path"
+  fi
+  echo "$path"
+}
+
+path_make_absolute() {
+  if [[ $1 && $1 != /* ]]; then
+    realpath "$1"
+  else
+    echo "$1"
+  fi
+}
