c981e047a5f04755c90e2f9f80f28aaa006313fc
Jan Pikl <pikl.jan@gmail.com>
2020-02-03 22:33:18 +0100

    Add pureline project_module

--- .local/lib/bash/prompt.sh
+++ .local/lib/bash/prompt.sh
@@ -5,15 +5,41 @@ readonly PURELINE_SCRIPT=$PURELINE_DIR/pureline
 readonly PURELINE_CONFIG=$CONFIG_DIR/pureline.conf
 
 if [[ -f $PURELINE_SCRIPT ]]; then
+  # shellcheck disable=SC2034
+  project_module() {
+    local content
+
+    if [[ -f pom.xml ]]; then
+      content="${PL_SYMBOLS[mvn]} mvn"
+      PS1+="$(section_end "$2" "$1")"
+      PS1+="$(section_content "$2" "$1" " $content ")"
+      __last_color="$1"
+    fi
+
+    if [[ -f package.json || -f yarn.lock ]]; then
+      if [ -f yarn.lock ]; then
+        content="${PL_SYMBOLS[yarn]} yarn"
+      else
+        content="${PL_SYMBOLS[npm]} npm"
+      fi
+      PS1+="$(section_end "$2" "$1")"
+      PS1+="$(section_content "$2" "$1" " $content ")"
+      __last_color="$1"
+    fi
+  }
+
   term_title_module() {
     local title="\033]0;"
     local project_root
+
     if project_root=$(findprj -au "$PWD" 2>/dev/null); then
       title+="[$(basename "$project_root")] "
     fi
+
     title+="$PWD\007"
     PS1+=$title
   }
+
   source "$PURELINE_SCRIPT" "$PURELINE_CONFIG"
 else
   {
