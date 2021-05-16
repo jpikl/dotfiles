cc9d75c188f05925610c29c93a81f7760390cf66
Jan Pikl <pikl.jan@gmail.com>
2021-05-16 20:26:55 +0200

    Run ssha/sshl in subshell

--- .bashrc.d/60-ssh.sh
+++ .bashrc.d/60-ssh.sh
@@ -6,6 +6,13 @@ alias sshi='eval "$(sshctl start)"'
 alias sshk='eval "$(sshctl stop)"'
 
 ## Select and add load SSH key
-alias ssha='cd ~/.ssh && find -name "id_*" -not -name "*.pub" -printf "%P\0" | choose -izp "SSH key:" | xargs -ro ssh-add; cd "$OLDPWD"'
+ssha() (
+  cd ~/.ssh || return 1
+  find . -name "id_*" -not -name "*.pub" -printf "%P\0" | choose -izp "SSH key:" | xargs -ro ssh-add
+)
+
 ## List loaded SSH keys
-alias sshl='cd ~/.ssh && fgrep -rHf <(ssh-add -L | cut -d" " -f2) | cut -d: -f1; cd "$OLDPWD"'
+sshl() (
+  cd ~/.ssh || return 1
+  grep -rHFf <(ssh-add -L | cut -d" " -f2) | cut -d: -f1
+)
