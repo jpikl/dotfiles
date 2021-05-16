998276b89bcc9f72af63026d6c2ae839923afdbc
Jan Pikl <pikl.jan@gmail.com>
2021-05-16 21:19:41 +0200

    Add mkdirpc alias

--- .bashrc.d/30-mkdir.sh
+++ .bashrc.d/30-mkdir.sh
@@ -2,3 +2,8 @@
 
 ## Make directory including its parent
 alias mkdirp='mkdir --parents'
+
+## Make directory including its parent and change into it
+mkdirpc() {
+  mkdirp "$1" && cd "$1" || return 1
+}
