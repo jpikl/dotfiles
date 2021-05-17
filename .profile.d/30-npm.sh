4eaef7e1a9e82ef77bbfa1ecb386630e94697bd2
Jan Pikl <pikl.jan@gmail.com>
2021-05-17 03:37:53 +0200

    Fix npm prefix for windows

--- .profile.d/30-npm.sh
+++ .profile.d/30-npm.sh
@@ -3,9 +3,9 @@
 # Main npm configuration
 export NPM_CONFIG_USERCONFIG=$USER_CONFIG_DIR/npm/npmrc
 
-# Global npm binaries should be installed to the $USER_BINARIES_DIR
-# which is already in PATH. On Windows, however, binaries are installed
-# to a different location.
+# Npm on Widows uses a different directory layout and has to be therefore
+# placed in different location rather than $USER_LOCAL_DIR configured in npmrc.
 if [[ ${MSYSTEM-} =~ ^MINGW(32|64)$ ]]; then
-  export PATH=$PATH:${USER_LOCAL_DIR}
+  export NPM_CONFIG_PREFIX=$USER_DATA_DIR/npm
+  export PATH=$PATH:$NPM_CONFIG_PREFIX
 fi
