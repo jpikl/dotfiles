f6567b29563d7527a8b40c9179a1fd49cc3b74b8
Jan Pikl <pikl.jan@gmail.com>
2021-05-23 01:34:01 +0200

    Move cargo and go dirs to ~/.local/share

--- .profile.d/40-cargo.sh
+++ .profile.d/40-cargo.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 
 # Cargo home directory
-export CARGO_HOME=$HOME/.cargo
+export CARGO_HOME=$USER_DATA_DIR/cargo
 
 # Update path for rust binaries
 export PATH=$PATH:$CARGO_HOME/bin
--- .profile.d/40-go.sh
+++ .profile.d/40-go.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 
 # Go workspace directory
-export GOPATH=$HOME/.go
+export GOPATH=$USER_DATA_DIR/go
 
 # Update path for go binaries
 export PATH=$PATH:$GOPATH/bin
