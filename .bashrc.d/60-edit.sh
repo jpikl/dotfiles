bc5c9f78c31a86ddf126264ec28f08f5a51b72bb
Jan Pikl <pikl.jan@gmail.com>
2021-05-25 00:50:13 +0200

    Cleanup profile.d scripts

--- .bashrc.d/60-edit.sh
+++ .bashrc.d/60-edit.sh
@@ -1,7 +1,11 @@
 # shellcheck shell=bash
 
+# We keep only the binary name from EDITOR variable.
+# The rest are additional flags/options which are unnecessary.
+# See ~/bashrc.d/20-editor.sh for more details.
+
 ## Open file with $EDITOR
-alias edit='$EDITOR_CMD'
+alias edit='${EDITOR/ *}'
 
 ## Read pipe with $EDITOR
-alias editpipe='xpipe $EDITOR_CMD'
+alias editpipe='xpipe ${EDITOR/ *}'
---
+++ .profile.d/20-editor.sh
@@ -0,0 +1,17 @@
+# shellcheck shell=bash
+
+if [[ ! $EDITOR ]]; then
+  EDITOR=$(find_executable
+    "sublime_text subl3 subl codium code"
+    "vim nano"
+  )
+
+  # If we want to use Sublime Text or Visual Studio Code as Git commit editor,
+  # they need their process to be foreground until they are actually closed.
+  if [[ $EDITOR =~ ^sublime_text|subl3|subl|code|codium$ ]]; then
+    EDITOR="$EDITOR --wait"
+  fi
+fi
+
+export EDITOR
+export VISUAL=$EDITOR
