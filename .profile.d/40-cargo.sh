bf7644ecd42a6beb03f79cddde845c187546527d
Jan Pikl <pikl.jan@gmail.com>
2021-05-29 21:11:20 +0200

    Add cargo config

--- .profile.d/40-cargo.sh
+++ .profile.d/40-cargo.sh
@@ -3,5 +3,8 @@
 # Cargo home directory
 export CARGO_HOME=$USER_DATA_DIR/cargo
 
+# Cargo install root directory
+export CARGO_INSTALL_ROOT=$USER_LOCAL_DIR
+
 # Update path for rust binaries
 export PATH=$PATH:$CARGO_HOME/bin
