162cf916c34f5b4296e91092a7ae906680b53ab4
Jan Pikl <pikl.jan@gmail.com>
2021-05-31 23:31:46 +0200

    Fix cargo install root

--- .profile.d/40-cargo.sh
+++ .profile.d/40-cargo.sh
@@ -1,7 +1,11 @@
 # shellcheck shell=bash
 
-# Cargo home directory
-export CARGO_HOME=${CARGO_HOME:-~/.cargo}
+# TODO: Use separate config/cache dirs once the following issue is resolved: https://github.com/rust-lang/cargo/pull/9178
+
+# Change the default CARGO_HOME install root to ~/.local
+export CARGO_INSTALL_ROOT=${CARGO_INSTALL_ROOT:-$USER_LOCAL_DIR}
 
 # Update path for rust binaries
-export PATH=$PATH:$CARGO_HOME/bin
+# CARGO_INSTALL_ROOT applies only to `cargo install`.
+# CARGO_HOME/bin might still contain rustup binaries.
+export PATH=$PATH:${CARGO_HOME:-~/.cargo}/bin
