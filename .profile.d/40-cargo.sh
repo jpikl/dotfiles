4c3419badc59a0f07f662f5b1c9f1ce85ec2a151
Jan Pikl <pikl.jan@gmail.com>
2021-05-31 08:37:49 +0200

    Use default cargo home

--- .profile.d/40-cargo.sh
+++ .profile.d/40-cargo.sh
@@ -1,11 +1,7 @@
 # shellcheck shell=bash
 
 # Cargo home directory
-export CARGO_HOME=$USER_DATA_DIR/cargo
-
-# Cargo install root directory
-export CARGO_INSTALL_ROOT=$USER_LOCAL_DIR
+export CARGO_HOME=${CARGO_HOME:-~/.cargo}
 
 # Update path for rust binaries
-# We changed install root but cargo home might still contain some rustup/cargo binaries.
 export PATH=$PATH:$CARGO_HOME/bin
