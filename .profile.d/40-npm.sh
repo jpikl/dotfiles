c5024dbfbb9365ffcb7690d5f124c4160e65adbf
Jan Pikl <pikl.jan@gmail.com>
2021-05-31 20:42:55 +0200

    Rewrite npm profile

--- .profile.d/40-npm.sh
+++ .profile.d/40-npm.sh
@@ -1,11 +1,14 @@
 # shellcheck shell=bash
 
-# Main npm configuration
-export NPM_CONFIG_USERCONFIG=$USER_CONFIG_DIR/npm/npmrc
-
-# Npm on Widows uses a different directory layout and has to be therefore
-# placed in different location rather than $USER_LOCAL_DIR configured in npmrc.
 if [[ ${MSYSTEM-} =~ ^MINGW(32|64)$ ]]; then
-  export NPM_CONFIG_PREFIX=$USER_DATA_DIR/npm
-  export PATH=$PATH:$NPM_CONFIG_PREFIX
+  # Npm on Windows uses a different directory layout inside %AppData%.
+  # Binaries are stored directly in the "prefix" directory.
+  export PATH=$PATH:${NPM_CONFIG_PREFIX:-$APPDATA/npm}
+else
+  # Change the defaut /usr/local prefix to ~/.local
+  export NPM_CONFIG_PREFIX=${NPM_CONFIG_PREFIX:-$USER_LOCAL_DIR}
+  # Change the default ~/.npm cache to ~/.cache/npm
+  export NPM_CONFIG_CACHE=${NPM_CONFIG_CACHE:-$USER_CACHE_DIR/npm}
+  # Update path for npm binaries
+  export PATH=$PATH:$NPM_CONFIG_PREFIX/bin
 fi
