ad7c4e6dbf249c7f71c6153b0ff109e12c180401
Jan Pikl <pikl.jan@gmail.com>
2021-05-31 20:43:54 +0200

    Update luarocks profile comment

--- .profile.d/40-luarocks.sh
+++ .profile.d/40-luarocks.sh
@@ -1,6 +1,7 @@
 # shellcheck shell=bash
 
-# Luarocks main configuration file
+# The default config file uses version suffix (e.g.: config-5.4.lua),
+# we want, however, a single configuration file for all lua versions.
 export LUAROCKS_CONFIG="$USER_CONFIG_DIR/luarocks/config.lua"
 
 # Update paths for lua
