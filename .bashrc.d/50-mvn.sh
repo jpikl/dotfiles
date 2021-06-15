fdab2e97c1230437692a41194f2731c2d1eb0434
Jan Pikl <pikl.jan@gmail.com>
2021-06-16 01:37:42 +0200

    Add xtable command

--- .bashrc.d/50-mvn.sh
+++ .bashrc.d/50-mvn.sh
@@ -13,7 +13,14 @@ alias mvncist='mvnci -Dmaven.test.skip=true'
 ## Maven dependency tree
 alias mvndeps='mvn dependency:tree'
 
-# Set version of a Maven project
+## Set version of a Maven project
 mvnsv() {
   mvn versions:set -DnewVersion="$1" -DgenerateBackupPoms=false --quiet
+}
+
+## Print property of a Maven project in Git branches.
+mvnpgb() {
+  local c1="git grep --fixed-strings '<$1>' '{}' -- pom.xml"
+  local c2="sed --regexp-extended 's/.*>(.*)<\/.*/\1/'"
+  git ls-branches -r | xtable -sI "$c1|$c2"
 }
