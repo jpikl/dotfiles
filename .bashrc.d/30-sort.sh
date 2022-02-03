731002b4984f232406a595a0845a8185f91c7bc8
Jan Pikl <pikl.jan@gmail.com>
2022-02-03 10:46:53 +0100

    Add sortuc alias

--- .bashrc.d/30-sort.sh
+++ .bashrc.d/30-sort.sh
@@ -2,3 +2,8 @@
 
 ## Sort lines, output unique results
 alias sortu='sort --unique'
+
+## Sort lines, output unique results with their count
+sortuc() {
+  sort | uniq --count | sort --numeric-sort --reverse
+}
