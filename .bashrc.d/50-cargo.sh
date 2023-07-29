a0a57797326e9aa60f0e185af6679bd8ab7eabbd
Jan Pikl <pikl.jan@gmail.com>
2023-07-29 22:28:11 +0200

    Cargo autocompletion

--- .bashrc.d/50-cargo.sh
+++ .bashrc.d/50-cargo.sh
@@ -1,4 +1,11 @@
 # shellcheck shell=bash
 
-## Simplified yarn audit
-alias yarnsa=$'yarn audit --json | jq \'select(.type == "auditAdvisory") | .data.advisory.module_name\' -r | sort -u'
+## rustup and cargo autocomplete
+if [[ -x $(command -v rustup) ]]; then
+    source <(rustup completions bash)
+    #source <(rustup completions bash cargo)
+    # Workaround for https://github.com/rust-lang/rustup/issues/3407
+    if [[ -f ~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/src/etc/bash_completion.d/cargo ]]; then
+        source ~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/src/etc/bash_completion.d/cargo
+    fi
+fi
