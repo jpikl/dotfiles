b6d0ebd57bdec139831cf87127f3a45c62b0b5b3
Jan Pikl <pikl.jan@gmail.com>
2023-12-17 20:19:20 +0100

    Update ssh agent initialization

--- .config/login.d/ssh.sh
+++ .config/login.d/ssh.sh
@@ -1,5 +1,8 @@
 # shellcheck shell=sh
 
+# Prefer our sshctl wrapper
 if [ -x "$(command -v sshctl)" ]; then
     eval "$(sshctl start 2>/dev/null)"
+elif [ -x "$(command -v ssh-agent)" ]; then
+    eval "$(ssh-agent -s)"
 fi
