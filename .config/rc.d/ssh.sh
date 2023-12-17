7a5286ef027c2f1510cf8af412361ae10b050e52
Jan Pikl <pikl.jan@gmail.com>
2023-12-17 18:46:40 +0100

    Fix ssh aliases

--- .config/rc.d/ssh.sh
+++ .config/rc.d/ssh.sh
@@ -1,6 +1,9 @@
 # shellcheck shell=sh
 
+# Start/stop agent
 alias sshi='eval "$(sshctl start)"'
 alias sshk='eval "$(sshctl stop)"'
-alias ssha='sshctl add'
-alias sshl='sshctl list'
+
+# Add/list keys
+alias ssha='find ~/.ssh -type f -name "id*" -not -name "*.pub" -print0 | fzf --read0 --print0 | xargs -r0 ssh-add'
+alias sshl='grep -Ff <(ssh-add -L) -R ~/.ssh'
