cab14487062bc9268ecb23d43c57cef30eaa1b11
Jan Pikl <pikl.jan@gmail.com>
2023-12-19 21:18:33 +0100

    Fix ssha/sshl aliases

--- .config/rc.d/ssh.sh
+++ .config/rc.d/ssh.sh
@@ -5,5 +5,5 @@ alias sshi='eval "$(sshctl start)"'
 alias sshk='eval "$(sshctl stop)"'
 
 # Add/list keys
-alias ssha='find ~/.ssh -type f -name "id*" -not -name "*.pub" -print0 | fzf --read0 --print0 | xargs -r0 ssh-add'
-alias sshl='grep -Ff <(ssh-add -L) -R ~/.ssh'
+alias ssha='find ~/.ssh -type f -name "id*" -not -name "*.pub" -print0 | fzf --exit-0 --select-1 --read0 --print0 | xargs -r0 ssh-add'
+alias sshl='grep -RFf <(ssh-add -L) ~/.ssh'
