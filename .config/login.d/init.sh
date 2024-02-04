845e7858a6450d09a623ba5cf7ba6ea031a25c98
Jan Pikl <pikl.jan@gmail.com>
2024-02-05 00:51:03 +0100

    Remove init and cleanup scripts

--- .config/login.d/init.sh
+++
@@ -1,9 +0,0 @@
-# shellcheck shell=sh
-# shellcheck disable=SC2164
-
-# Switch to home directory
-cd ~
-
-# Clear terminal and reset its state
-clear
-tput reset
--- .config/logout.d/cleanup.sh
+++
@@ -1,5 +0,0 @@
-# shellcheck shell=sh
-
-# Clear terminal and reset its state
-clear
-tput reset
