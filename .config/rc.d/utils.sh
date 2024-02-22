7826419929d0024358a135e54a2c1fbb41e2fd45
Jan Pikl <pikl.jan@gmail.com>
2024-02-22 11:58:30 +0100

    Drop custom pager in favor of less

--- .config/rc.d/utils.sh
+++ .config/rc.d/utils.sh
@@ -8,6 +8,7 @@ alias sortu='sort -u'
 alias wcl='wc -l'
 alias cutw='cut -c -$COLUMNS'  # Cut lines to fit terminal width
 alias lspath='echo $PATH | tr : "\n"' # List PATH entries as lines
+alias page='less -FIrX'
 
 # Sort lines, output unique results with their count
 sortuc() {
