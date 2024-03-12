e640440c16605fe97e3491df5271b52c3c12d846
Jan Pikl <pikl.jan@gmail.com>
2024-03-12 12:16:01 +0000

    Fix ssh key generation on windows

--- .config/admin.d/generate-ssh-key.sh
+++ .config/admin.d/generate-ssh-key.sh
@@ -11,11 +11,11 @@ w) KEY_FILE=~/.ssh/id_work ;;
 *) exit 1 ;;
 esac
 
-DEFAULT_COMMENT=$USER@$(hostname)
+DEFAULT_COMMENT=$(whoami)@$(hostname)
 printf "Enter comment (empty for %s): " "$DEFAULT_COMMENT"
 read -r COMMENT
 ssh-keygen -t ed25519 -f "$KEY_FILE" -a 100 -C "${COMMENT:-$DEFAULT_COMMENT}"
 echo
 echo "Copy the following public key to a remote server:"
 echo
-cat "$KEY_FILE"
+cat "$KEY_FILE.pub"
